35f0177b286ad89b87ec948a39d97e8def0e0fe9
Mohamed Seif El Islam Benatallah <mohamed.benatallah@univ-constantine2.dz>
2026-02-05 21:33:52 +0100

    created the user model updated the mongoose version for compatibility.

--- server.ts
+++ server.ts
@@ -3,16 +3,16 @@ import connectDB from "./utils/db";
 require ("dotenv").config();
 import {v2 as cloudinary} from "cloudinary";
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.CLOUD_API_KEY,
-//     api_secret: process.env.CLOUD_SECRET_KEY
-// });
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_SECRET_KEY
+});
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port,()=>{
     console.log(`app is running on port ${port}`);
-    // connectDB();
+    connectDB();
 });
 
 process.on("SIGINT", () => {
